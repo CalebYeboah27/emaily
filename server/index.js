b018749581c8b87c56b64bf3e0071218b776d7f1
Caleb Yeboah <caleb.yeboah@amalitech.org>
2023-07-08 19:23:02 +0000

    testing callback url

--- server/index.js
+++ server/index.js
@@ -1,9 +1,47 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const { googleClientID, googleClientSecret } = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      // authorizationURL: 'https://www.google.com/oauth2/authorize',
+      // tokenURL: 'https://www.google.com/oauth2/token',
+      clientID: googleClientID,
+      clientSecret: googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      // User.findOrCreate({ exampleId: profile.id }, function (err, user) {
+      //   return cb(err, user);
+      // });
+      console.log(accessToken, refreshToken, profile, cb);
+    }
+  )
+);
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT)
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  })
+);
+
+try {
+  app.get(
+    'auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function (req, res) {
+      // Successful authentication, redirect home.
+      res.redirect('/');
+    }
+  );
+} catch (error) {
+  console.log(error);
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
